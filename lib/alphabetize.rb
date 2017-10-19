b10680ce2d008e93343e7dd6eb9d57f692ed2220
Josh-Stillman <josh.stillman@gmail.com>
2017-10-19 11:50:02 -0400

    done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  e_array = esperanto.split("")
+
+  return_array = arr.sort_by do |word|
+    i = 0
+    sort_array = []
+    while i < word.length
+      sort_array.push(e_array.index(word[i]))
+      i += 1
+    end
+    sort_array
+  end
+  return_array
 end
